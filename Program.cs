6d1e231b90a1a6a24812656de1ac3adc71306eaf
Lorraine Warren <lorrywarren@gmail.com>
2025-05-02 21:20:40 +0100

    Route all of the methods from Db.cs to the program file. Added the namespace so the Db cs file could be used in the program file. Mapped routes to the methods.

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 using Microsoft.OpenApi.Models;
+using PizzaStore.DB;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddEndpointsApiExplorer();
@@ -19,5 +20,16 @@ if (app.Environment.IsDevelopment())
 }
 
 app.MapGet("/", () => "Hello World!");
+// Map to route - Get a pizza by its Id
+app.MapGet("/pizzas/{id}",(int id) => PizzaDB.GetPizza(id));
+// Map to route - Get all the pizzas
+app.MapGet("/pizzas",() => PizzaDB.GetPizzas());
+// Map to route - Create a new pizza entry
+app.MapPost("/pizzas", (Pizza pizza) => PizzaDB.CreatePizza(pizza));
+// Map to route - Update pizza entry
+app.MapPut("/pizzas", (Pizza Pizza) => PizzaDB.UpdatePizza(Pizza));
+// Map to route - Delete a puzza entry by Id
+app.MapDelete("/pizzas/{id}", (int id) => PizzaDB.RemovePizza(id));
+
 
 app.Run();
